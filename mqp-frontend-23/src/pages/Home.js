2014dc87e47b2545ebae20febba7ec34cd41e6ea
gearymich <gearymich@gmail.com>
2022-12-30 03:33:46 -0500

    improved appbar, globalized theme/styling

--- mqp-frontend-23/src/pages/Home.js
+++ mqp-frontend-23/src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import {Button, Switch, CssBaseline, Typography, AppBar, Card, CardActions, CardMedia, Grid, Toolbar, Container } from '@mui/material';
+
 function Home() {
   return (
-    <div>
-    Hello World!
-    </div>
+    <main>
+      <div>
+        <Container maxWidth="sm">
+          <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            Photo Album
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Hello everyone! This is a photo album and I'm trying to make this line long to see what will happen to the
+            layout on different screen sizes.
+          </Typography>
+        </Container>
+      </div>
+    </main>
   );
 }
 
