87fab2f4b375524bc878567e3151d3c88bca26df
gearymich <gearymich@gmail.com>
2023-02-16 15:40:12 -0500

    Updates

---
+++ precache-manifest.fd9540c8c23f301e28751886d192ef47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0da0c69c5c6bd48171d3",
+    "url": "/react-gh-pages/static/css/main.1faaf499.chunk.css"
+  },
+  {
+    "revision": "0da0c69c5c6bd48171d3",
+    "url": "/react-gh-pages/static/js/main.0da0c69c.chunk.js"
+  },
+  {
+    "revision": "d5ac269844caaa3d6dc0",
+    "url": "/react-gh-pages/static/js/1.d5ac2698.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "89d073624e4d4851cb136881633b054e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
