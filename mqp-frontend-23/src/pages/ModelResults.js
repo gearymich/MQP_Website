13d155c4b0ea1731166ec5b29bfc28ea242938db
gearymich <gearymich@gmail.com>
2023-03-03 08:48:16 -0500

    added reduced + full dataset charts

--- mqp-frontend-23/src/pages/ModelResults.js
+++ mqp-frontend-23/src/pages/ModelResults.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Grid } from '@mui/material';
+
 import ModelTable from '../components/ModelTable';
 import LabelTable from '../components/LabelTable';
 
@@ -8,6 +10,25 @@ import './ModelResults.css';
 const ModelResults = () => {
   return (
     <main className='table-container'>
+        <div className="global-padding home-section">
+        <h2> Reduced Dataset + Full Dataset Model Metrics </h2>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ sm: 8, md: 12 }} className="grid-format">
+          <Grid item xs={5} sm={6}>
+          <img
+              src="./full-dataset-results-barchart.png"
+              alt= "Full Dataset Results Bargraph"
+              className="elephant-img"
+              />
+          </Grid>
+          <Grid item xs={5} sm={6}>
+            <img
+              src="./reduced-dataset-results-barchart.png"
+              alt= "Reduced Dataset Results Bargraph"
+              className="elephant-img"
+              />
+          </Grid>
+        </Grid>
+      </div>
       <h2> 2023 MQP Baselines + Proposed Models </h2>
       <div>     
         <ModelTable/>
